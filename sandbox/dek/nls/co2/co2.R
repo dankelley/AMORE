e01ab3c14e10252d3c9cc2d781e1726b8d68574e
dankelley <kelley.dan@gmail.com>
2023-05-17 15:47:45 -0300

    meeting 3 minutes and extras

---
+++ sandbox/dek/nls/co2/co2.R
@@ -0,0 +1,16 @@
+par(mar=c(3,3,1,1), mgp=c(2,0.7,0)) # use thin margins
+plot(co2)
+#par(mfrow=c(3, 1))
+t <- as.numeric(time(co2))
+tt <- t - t[1]
+plot(t, co2, type="l")
+m1 <- nls(co2 ~ A+B*exp((t-1950)/tau), start=list(A=300, B=10, tau=10))
+lines(t, predict(m1), col=2)
+plot(t, co2 - predict(m1), type="l")
+S <- sin(2 * pi * t / 1)
+S2 <- sin(2 * pi * t / 0.5)
+C <- cos(2 * pi * t / 1)
+C2 <- cos(2 * pi * t / 0.5)
+co2B <- co2 - predict(m1)
+m2 <- lm(co2B ~ S + C + S2 + C2)
+plot(t, co2 - predict(m1) - predict(m2), type="l")
