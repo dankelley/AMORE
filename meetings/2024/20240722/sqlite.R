44a48d6e61b03784c5ac7c236b4e87683b4d9a91
dankelley <kelley.dan@gmail.com>
2024-07-22 13:48:03 -0300

    demo working with netcdf and sqlite3 files

--- meetings/2024/20240722/sqlite.R
+++ meetings/2024/20240722/sqlite.R
@@ -1,22 +1,28 @@
+# Demonstrating (a) writing to SQLITE3 database and (b) reading back
+# from that database.
 library(DBI)
 library(RSQLite)
+
+# First, create some data
 x <- seq(0, 4, 0.1)
 y <- x * exp(-x)
 plot(x, y)
 df <- data.frame(x = x, y = y)
 
-# we will write to this file (remove it first, if it exists
+# We will write to this file (remove it first, if it exists
 # from previous testing)
-f <- "sqlite_demo.db"
+f <- "sqlite.db"
 unlink(f)
 
-# demo writing
+# PART a: writing
 con <- dbConnect(RSQLite::SQLite(), dbname = f)
 dbWriteTable(con, "xy", df)
 dbDisconnect(con)
 
-# demo reading
+# PART b: reading
 CON <- dbConnect(RSQLite::SQLite(), dbname = f)
 DF <- dbReadTable(CON, "xy")
 dbDisconnect(CON)
-points(DF$x, DF$y, col = 2, cex=2)
+
+# PART c: check results
+stopifnot(identical(df, DF))
